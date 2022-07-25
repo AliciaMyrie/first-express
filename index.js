687eeea43be8f1d6094294517881bfe5cd5afca6
Alicia Myrie <aliciamyrie@gmail.com>
2022-07-25 16:48:22 -0400

    second edit

--- index.js
+++ index.js
@@ -13,7 +13,30 @@ const app = express();
 // list requests that we want express to respond to
 app.get("/hello", (request, response) => {
   // do something ..
+  console.log("xxx Hello World! xxx");
   response.send("Hello World!");
+});
+
+const students = [
+  { firstName: "Jonathon", lastName: "Vagas" },
+  { firstName: "Alicia", lastName: "Myrie" },
+  { firstName: "Bridgette", lastName: "Lemus" },
+  { firstName: "Mason", lastName: "Madaras" },
+];
+
+app.get("/students", (request, response) => {
+  response.send(students); // sends response back to Chrome
+});
+
+app.get("/students/:firstName", (request, response) => {
+  const student = students.find(
+    (stud) => stud.firstName === request.params.firstName
+  );
+  if (!student) {
+    response.status(404).send({ message: "Student not found", success: false });
+    return;
+  }
+  response.send(student);
 });
 
 // start listening on a port
